ba09bdd99998a4d61e71efc191bd5ecdd29e45a3
Madeleine <mahdieh.moghadasi@gmail.com>
2021-11-08 08:57:21 -0500

    amend transition fn

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,26 +6,23 @@ function useVisualMode (initial) {
     const [mode, setMode] = useState(initial);
     const [history, setHistory] = useState([initial]);
 
-    function transition (newMode, replace = false) {
-        //If there is an error, places the new mode in the correct position
-        if (replace) {
-          setMode((prev) => newMode)
-          let replaceHistory = [...history];
-          replaceHistory[replaceHistory.length - 1] = mode;
-          setHistory((prev) => replaceHistory);
-        } else {
-          setMode((prev) => newMode);
-          let newHistory = [...history];
-          newHistory.push(newMode);
-          setHistory((prev) => newHistory);
-        }
-  };
-    function back () {
-        if (history.length === 1 ) return;
-        let historyStack = history.slice(0,history.length-1)
-        setHistory ([...historyStack])
-        setMode(history[history.length-2])
-    }
+    const transition = (newMode, replaceCurrentMode = false) => {
+      setMode(newMode);
+      setHistory((prev) => {
+        const prevState = [...prev];
+        replaceCurrentMode && prevState.pop();
+        return [...prevState, newMode];
+      });
+    };
+    const back = () => {
+      if (history.length > 1) {
+        const newHistory = [...history];
+        newHistory.pop();
+        setMode(newHistory[newHistory.length - 1]);
+        setHistory(newHistory);
+      }
+    };
+  
     return { mode , transition, back }
 }
 
